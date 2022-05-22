212a68ec685576bc41bce5595ff5c53a6a46fece
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2022-05-22 16:25:02 +0300

    (update) - backend reg done

--- routes/weapon.js
+++ routes/weapon.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/user-controller');
 const weaponController = require('../controllers/weapon-controller');
 const router = Router();
 const {body} = require('express-validator');
+const authMiddleware = require('../middleware/auth-middleware');
 
 
 // pages
@@ -30,7 +31,7 @@ router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
-router.get('/users', userController.getUsers);
+router.get('/users', authMiddleware, userController.getUsers);
 
 
 module.exports = router;
