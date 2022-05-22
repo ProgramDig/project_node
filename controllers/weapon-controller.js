39e5e99b07bd3137df818a78792f02d87aa92524
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2022-05-22 11:09:56 +0300

    (fix) - error delete

---
+++ controllers/weapon-controller.js
@@ -0,0 +1,80 @@
+const Weapon = require('../model/Weapon');
+
+
+class WeaponController {
+	async getWeapon(req, res) {
+		try{
+			const weapons = await Weapon.find({});
+			res.render('index', {
+				title: 'Main page',
+				message: 'Second company',
+				weapons
+			})
+		} catch (e) {
+			console.log(`Error: ${e}`)
+			res.json(e)
+		}
+	}
+
+ 	getCreate (req, res){
+		res.render('create', {
+			title: 'Create page',
+			message: 'Second company'
+		})
+	}
+
+	getUpdate (req, res) {
+		res.render('update', {
+			title: 'Update page',
+			message: 'Second company'
+		})
+	}
+
+	getDelete (req, res) {
+		res.render('delete', {
+			title: 'Delete page',
+			message: 'Second company'
+		})
+	}
+
+	async createWeapon(req, res){
+		try {
+			const weapon = new Weapon({
+				id: req.body.id,
+				name: req.body.name,
+				number: req.body.number,
+				platoon: req.body.platoon,
+				readiness: req.body.readiness
+			})
+			await weapon.save();
+		} catch (e) {
+			console.log(`Error: ${e}!`);
+			res.status(500).json(e);
+		}
+		res.redirect('/main');
+	}
+
+	async deleteWeapon(req, res) {
+		const id = req.body.id;
+		await Weapon.deleteOne({id}, () => {
+			console.log(`Weapon with id ${id} deleted !`);
+			res.status(200).redirect('/main');
+		}).clone().catch((e) => {
+			console.log(`Error: ${e}!`);
+			res.status(500).json(e);
+		});
+	}
+
+	async updateWeapon(req, res){
+		try{
+			const id = req.body.id;
+			await Weapon.updateOne({id}, req.body);
+			res.redirect('/main');
+		} catch (e){
+			console.log(`Error: ${e}!`);
+			res.status(500).json(e);
+		}
+	}
+}
+
+module.exports = new WeaponController();
--- routes/weapon.js
+++ routes/weapon.js
@@ -2,82 +2,22 @@ const { cache } = require('ejs');
 const {Router} = require('express');
 const Weapon = require('../model/Weapon')
 const userController = require('../controllers/user-controller');
+const weaponController = require('../controllers/weapon-controller');
 const router = Router();
 
 
 // pages
-
-router.get('/main', async (req, res) => {
-	try{
-		const weapons = await Weapon.find({});
-		res.render('index', {
-			title: 'Main page',
-			message: 'Second company',
-			weapons
-		})
-	} catch (e) {
-		console.log(`Error: ${e}`)
-	}
-});
-
-router.get('/create', (req, res) => {
-	res.render('create', {
-		title: 'Create page',
-		message: 'Second company'
-	})
-});
-
-router.get('/update', (req, res) => {
-	res.render('update', {
-		title: 'Update page',
-		message: 'Second company'
-	})
-});
-
-router.get('/delete', (req, res) => {
-	res.render('delete', {
-		title: 'Delete page',
-		message: 'Second company'
-	})
-});
+router.get('/main', weaponController.getWeapon);
+router.get('/create', weaponController.getCreate);
+router.get('/update', weaponController.getUpdate);
+router.get('/delete', weaponController.getDelete);
 
 // CREATE
-router.post('/create', async (req, res) => {
-	try {
-		const weapon = new Weapon({
-			id: req.body.id,
-			name: req.body.name,
-			number: req.body.number,
-			platoon: req.body.platoon,
-			readiness: req.body.readiness
-		})
-		await weapon.save();
-	} catch (e) {
-		console.log(`Error: ${e}!`);
-		res.status(500).json(e);
-	}
-	
-	res.redirect('/main');
-});
+router.post('/create', weaponController.createWeapon);
 // DELETE
-router.post('/delete/:id', async (req, res) => {
-	try{
-		const id = req.body.id;
-		await Weapon.remove({id}, () => {
-			console.log(`Weapon with id ${id} deleted !`)
-		})
-		res.status(200).redirect('/main');
-	} catch (e) {
-		console.log(`Error: ${e}!`);
-		res.status(500).json(e);
-	}
-});
+router.post('/delete/:id', weaponController.deleteWeapon);
 // UPDATE
-router.post('/update/:id', async (req, res) => {
-	const id = req.body.id;
-	await Weapon.updateOne({id}, req.body);
-	res.redirect('/main');
-});
+router.post('/update/:id', weaponController.updateWeapon);
 
 // authorization
 
