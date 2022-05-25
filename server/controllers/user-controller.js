774f1f847cd88ff620ee067cc752dfa3a2c8c963
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2022-05-25 19:07:36 +0300

    (test-1)

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -12,8 +12,8 @@ class UserController {
 			const {email, password} = req.body;
 			const userData = await userService.registration(email, password);
 
-			res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
-			return res.json(userData);
+			res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true}).redirect('/login');
+			// return res.json(userData);
 		} catch (e) {
 			next(e);
 		}
@@ -23,8 +23,8 @@ class UserController {
 		try{
 			const {email, password} = req.body;
 			const userData = await userService.login(email, password);
-			res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
-			return res.json(userData);
+			res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true}).redirect('/main');
+			// return res.json(userData);
 		} catch (e) {
 			next(e);
 		}
